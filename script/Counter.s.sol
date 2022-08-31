eedc35524dc6257575bbdd27b9773f24e2eff465
Hans Bergren <hansbergren@gmail.com>
2022-08-31 12:15:11 -0700

    fix script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    function setUp() external {}
 
-    function run() public {
+    function run() external {
         vm.broadcast();
     }
 }
